478c791fbe63ad80db2dd7d4161c20a7de89e58e
carlosziegler <carlos.ziegler@gmail.com>
2020-02-10 16:40:26 +0100

    #31
    Functional Component

---
+++ 27/react-complete-guide/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Person from './Person'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi , I am a React app</h1>
+        <Person></Person>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 27/react-complete-guide/src/Person/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person = () => {
+    return <p>Hi, I am a Person</p>
+}
+
+export default Person
